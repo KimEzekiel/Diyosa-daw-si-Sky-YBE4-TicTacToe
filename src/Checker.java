8950ce2a3a85b4dbf915bdd12c0ad3dbc9a12013
DietherReyes <cbreyes4@up.edu.ph>
2017-04-17 21:50:08 +0800

    Added Checkers

--- src/Checker.java
+++ src/Checker.java
@@ -1,40 +1,8 @@
-class Checker implements runnable{
+class Checker{
   private Player player;
   private TicTacToe tictactoe;
   public Checker(Player player, TicTacToe tictactoe){
     this.player = player;
     this.tictactoe = tictactoe;
-  }
-  public void run(){
-    int[][] board = tictactoe.getBoard();
-    if(board[0][0].equals(board[0][1]) && board[0][0].equals(board[0][2])){   //---------
-      player.setScore();                                                      //        -
-    }                                                                         //        -
-    if(board[1][0].equals(board[1][1]) && board[1][0].equals(board[1][2])){   //        -
-      player.setScore();                                                      //        -----> horizontal checker
-    }                                                                         //        -
-    if(board[2][0].equals(board[2][1]) && board[2][0].equals(board[2][2])){   //        -
-      player.setScore();                                                      //        -
-    }                                                                         //---------
-
-    if(board[0][0].equals(board[1][0]) && board[0][0].equals(board[2][0])){   //---------
-      player.setScore();                                                      //        -
-    }                                                                         //        -
-    if(board[0][1].equals(board[1][1]) && board[0][1].equals(board[2][1])){   //        -
-      player.setScore();                                                      //        -----> Vertical checker
-    }                                                                         //        -
-    if(board[0][2].equals(board[1][2]) && board[0][2].equals(board[2][2])){   //        -
-      player.setScore();                                                      //        -
-    }                                                                         //---------
-
-
-
-    if(board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2])){   //---------
-      player.setScore();                                                      //        -
-    }                                                                         //        -
-    if(board[2][0].equals(board[1][1]) && board[2][0].equals(board[0][2])){   //        -----> Diagonal Checker
-      player.setScore();                                                      //        -
-    }                                                                         //---------
-
   }
 }
