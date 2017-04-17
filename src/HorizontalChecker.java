40cdbdaa77de1035276f561514193cb3231336b9
DietherReyes <cbreyes4@up.edu.ph>
2017-04-17 22:42:47 +0800

    Comment out lines

--- src/HorizontalChecker.java
+++ src/HorizontalChecker.java
@@ -6,13 +6,13 @@ class HorizontalChecker implements runnable{
   void run(){
     char[][] board = tictactoe.getBoard();
     if(board[0][0].equals(board[0][1]) && board[0][0].equals(board[0][2])){   //---------
-      player.setScore();                                                      //        -
+      //player.setScore();                                                      //        -
     }                                                                         //        -
     if(board[1][0].equals(board[1][1]) && board[1][0].equals(board[1][2])){   //        -
-      player.setScore();                                                      //        -----> horizontal checker
+      //player.setScore();                                                      //        -----> horizontal checker
     }                                                                         //        -
     if(board[2][0].equals(board[2][1]) && board[2][0].equals(board[2][2])){   //        -
-      player.setScore();                                                      //        -
+      //player.setScore();                                                      //        -
     }                                                                         //---------
   }
 }
