eddfd1add24853ff5bc4d430b334423ee844fe78
KimEzekiel <kldelmundo@up.edu.ph>
2017-04-21 22:52:09 +0800

    check if correct

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,6 +9,7 @@ class TicTacToe{
      private int currPlayerCount;
      private int currentRound;
      private int currTurnNumber;
+     private Player winner;
 
      public TicTacToe(int rounds, Player player1, Player player2){
           this.rounds = rounds;
@@ -46,7 +47,9 @@ class TicTacToe{
      public int getCurrentTurn(){
           return this.currTurnNumber;
      }
-
+     public Player getWinner(){
+          return this.winner;
+     }
      /*Setters*/
      public void setRounds(int rounds){
           this.rounds = rounds;
@@ -57,42 +60,56 @@ class TicTacToe{
      public void setCurrentTurn(int turn){
 
      }
+/*
      public void addToGame(Player newPlayer){
           if(this.currPlayerCount  < PLYER_NUM){
                this.players[this.currPlayerCount] = newPlayer;
           }
      }
-
+*/
      public void playerMoves(int x, int y){
-          if(this.currTurnNumber <=9 ){
-               if(this.currentRound%2 == 0){ //if currentRound is even
-                    if(this.currTurnNumber%2 == 0){    //currTurnNumber is even
-                         System.out.println("Player2 moves");
-                         this.players[1].mark(x,y);
-                    }else{
-                          this.players[0].mark(x,y);
-                          System.out.println("Player1 moves");
-                     }
-               }else{    //currentRound is odd
-                    if(this.currTurnNumber%2 == 0){    //currTurnNumber is even
-                         this.players[0].mark(x,y);
-                         System.out.println("Player1 moves");
-                    }else {
-                         this.players[1].mark(x,y);
-                         System.out.println("Player2 moves");
+          if(this.currentRound <= this.rounds){   //game is ongoing
+               if(this.currTurnNumber <= 9){ //a round is ongoing
+                    if(this.currentRound%2 == 0){ //if currentRound is even
+                         if(this.currTurnNumber%2 == 0){    //currTurnNumber is even
+                              System.out.println("Player2 moves");
+                              this.players[1].mark(x,y);
+                         }else{
+                              this.players[0].mark(x,y);
+                              System.out.println("Player1 moves");
+                         }
+                    }else{    //currentRound is odd
+                         if(this.currTurnNumber%2 == 0){    //currTurnNumber is even
+                              this.players[0].mark(x,y);
+                              System.out.println("Player1 moves");
+                         }else {
+                              this.players[1].mark(x,y);
+                              System.out.println("Player2 moves");
+                         }
                     }
+               }else{//end of round
+                    //start next round
+                    this.currentRound ++;
+                    this.currTurnNumber = 1; //back to round 1
                }
+          }else{     //end of game, get winner
+
+          }
+
+          if(this.currTurnNumber <=9 ){
           }if(this.currTurnNumber >=10){
                this.currentRound++;
                this.currTurnNumber = 1;
           }
      }
 
-     public void play(int x, y){
+
+     public void play(int x,int  y){
           for(this.currentRound; this.currentRound <= this.rounds; this.currentRound++){  //for each round
                this.playerMoves(x,y);
           }
      }
+
 
      public void printBoard(){
           for(int i = 0; i < BOARD_HGHT; i++){
