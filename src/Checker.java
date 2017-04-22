3057d03ca9077fe01f44f91c0b50a1c48135e963
KimEzekiel <kldelmundo@up.edu.ph>
2017-04-23 00:51:25 +0800

    created demo

--- src/Checker.java
+++ src/Checker.java
@@ -5,7 +5,7 @@ class Checker{
   }
 
   void checkPlayer(char c){
-  
+     /*adds score for the player who owns a the symbol c*/
     if(c == '0') tictactoe.getPlayers()[0].addScore();
     else if(c == 'x')tictactoe.getPlayers()[1].addScore();
   }
---
+++ src/Demo.java
@@ -0,0 +1,9 @@
+
+public class Demo{
+     /*
+     This class is a clas for testing things out
+     */
+     public static void main(String[] args){
+
+     }
+}
