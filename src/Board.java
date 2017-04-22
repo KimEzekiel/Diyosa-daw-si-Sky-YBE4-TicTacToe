0817831883042b52b22268c42394b0489e255743
Kotomi0226 <jttagaza@up.edu.ph>
2017-04-22 22:58:38 +0800

    ImproveUI

--- src/Board.java
+++ src/Board.java
@@ -6,16 +6,12 @@ import javax.swing.JLabel;
 import java.awt.Dimension;
 import java.awt.Container;
 import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.FlowLayout;
 import java.awt.CardLayout;
 
-public class Board extends JFrame{
-  private JFrame frame;
+class Board extends JFrame{
 
   public Board(String name){
     super(name);
@@ -27,36 +23,54 @@ public class Board extends JFrame{
     this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 
     Container container = this.getContentPane();
-    container.setLayout(new GridLayout(3,1));
 
     JPanel board = new JPanel();
     board.setLayout(new GridLayout(3,3));
-    JPanel centerPanel = new JPanel();
-    centerPanel.setLayout(new GridLayout(1,3));
+    JPanel northPanel = new JPanel();
+    northPanel.setPreferredSize(new Dimension(500,50));
+    northPanel.setLayout(new FlowLayout());
+    northPanel.setBackground(Color.YELLOW);
     JPanel westPanel = new JPanel();
-    westPanel.setLayout(new GridLayout(1,1));
+    westPanel.setPreferredSize(new Dimension(125,300));
+    westPanel.setBackground(Color.YELLOW);
     JPanel eastPanel = new JPanel();
-    eastPanel.setLayout(new GridLayout(1,1));
+    eastPanel.setPreferredSize(new Dimension(175,300));
+    eastPanel.setBackground(Color.YELLOW);
+    JPanel southPanel = new JPanel();
+    southPanel.setPreferredSize(new Dimension(500,50));
+    southPanel.setBackground(Color.YELLOW);
+
+    JLabel player1 = new JLabel();
+    player1.setFont(player1.getFont().deriveFont(15f));
+    player1.setText("Player 1: 0");
+    player1.setPreferredSize(new Dimension(155,50));
+    JLabel turn = new JLabel();
+    turn.setFont(player1.getFont().deriveFont(15f));
+    turn.setText("Player 1's turn");
+    turn.setPreferredSize(new Dimension(155,50));
+    JLabel player2 = new JLabel();
+    player2.setText("Player 2: 0");
+    player2.setFont(player2.getFont().deriveFont(15f));
+    player2.setPreferredSize(new Dimension(150,50));
 
     JButton topLeft = new JButton();
+    topLeft.setFont(topLeft.getFont().deriveFont(15f));
     JButton topMiddle = new JButton();
+    topMiddle.setFont(topMiddle.getFont().deriveFont(15f));
     JButton topRight = new JButton();
+    topRight.setFont(topRight.getFont().deriveFont(15f));
     JButton middleLeft = new JButton();
+    middleLeft.setFont(middleLeft.getFont().deriveFont(15f));
     JButton center = new JButton();
+    center.setFont(center.getFont().deriveFont(15f));
     JButton middleRight = new JButton();
+    middleRight.setFont(middleRight.getFont().deriveFont(15f));
     JButton bottomLeft = new JButton();
+    bottomLeft.setFont(bottomLeft.getFont().deriveFont(15f));
     JButton bottomMiddle = new JButton();
+    bottomMiddle.setFont(bottomMiddle.getFont().deriveFont(15f));
     JButton bottomRight = new JButton();
-
-    JLabel northLabel = new JLabel();
-    northLabel.setText("              Player 1's turn");
-    JLabel westLabel = new JLabel();
-    westLabel.setLayout(new GridLayout());
-    westLabel.setText("Player 1 ->");
-    JLabel eastLabel = new JLabel();
-    eastLabel.setLayout(new GridLayout());
-    eastLabel.setText("Player 2 ->");
-    JLabel southLabel = new JLabel();
+    bottomRight.setFont(bottomRight.getFont().deriveFont(15f));
 
     board.add(topLeft);
     board.add(topMiddle);
@@ -68,20 +82,28 @@ public class Board extends JFrame{
     board.add(bottomMiddle);
     board.add(bottomRight);
 
-    westPanel.add(westLabel);
+    northPanel.add(player1);
+    northPanel.add(turn);
+    northPanel.add(player2);
 
-    centerPanel.add(northLabel);
-    centerPanel.add(board);
-    centerPanel.add(southLabel);
+    container.add(northPanel, BorderLayout.NORTH);
+    container.add(westPanel, BorderLayout.WEST);
+    container.add(board, BorderLayout.CENTER);
+    container.add(eastPanel, BorderLayout.EAST);
+    container.add(southPanel, BorderLayout.SOUTH);
 
-    eastPanel.add(eastLabel);
-
-    container.add(westPanel);
-    container.add(centerPanel);
-    container.add(eastPanel);
-
+    topLeft.addMouseListener(new BoardMouseListener(topLeft));
+    topMiddle.addMouseListener(new BoardMouseListener(topMiddle));
+    topRight.addMouseListener(new BoardMouseListener(topRight));
+    middleLeft.addMouseListener(new BoardMouseListener(middleLeft));
+    center.addMouseListener(new BoardMouseListener(center));
+    middleRight.addMouseListener(new BoardMouseListener(middleRight));
+    bottomLeft.addMouseListener(new BoardMouseListener(bottomLeft));
+    bottomMiddle.addMouseListener(new BoardMouseListener(bottomMiddle));
+    bottomRight.addMouseListener(new BoardMouseListener(bottomRight));
 
     this.pack();
+    this.setLocationRelativeTo(null);
     this.setVisible(true);
   }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,9 +9,11 @@ class TicTacToe{
      private int currPlayerCount;
      private int currentRound;
      private int currTurnNumber;
+     private Player winner;
 
      public TicTacToe(int rounds, Player player1, Player player2){
           this.rounds = rounds;
+          this.board = new char[BOARD_HGHT][BOARD_WDTH];
           for(int i = 0; i < BOARD_HGHT;i ++ ){
                for(int j = 0; j < BOARD_WDTH; j++){
                     this.board[i][j] = ' ';
@@ -21,7 +23,9 @@ class TicTacToe{
           this.players[0] = player1;
           this.players[1] = player2;
           this.currentRound = 1;
-          this.currPlayerCount = 0;
+          this.currPlayerCount = 2;
+          player1.setTicTacToe(this);
+          player2.setTicTacToe(this);
 
      }
      /*Getters*/
@@ -40,10 +44,12 @@ class TicTacToe{
      public int getCurrentPlayerCount(){
           return this.currPlayerCount;
      }
-     public int getCurrentTurnNumber(){
+     public int getCurrentTurn(){
           return this.currTurnNumber;
      }
-
+     public Player getWinner(){
+          return this.winner;
+     }
      /*Setters*/
      public void setRounds(int rounds){
           this.rounds = rounds;
@@ -51,22 +57,63 @@ class TicTacToe{
      public void setCurrentRound(int round){
           this.currentRound = round;
      }
+     public void setCurrentTurn(int turn){
+          this.currTurnNumber += turn;
+     }
+/*
      public void addToGame(Player newPlayer){
           if(this.currPlayerCount  < PLYER_NUM){
                this.players[this.currPlayerCount] = newPlayer;
           }
      }
+*/
      public void playerMoves(int x, int y){
-          if(this.currentRound%2 == 0){ //if currentRound is even
-               if(this.currTurnNumber%2 == 0){    //currTurnNumber is even
-                    this.players[1].mark(x,y);
-               }else this.players[0].mark(x,y);
-          }else{
-               if(this.currTurnNumber%2 == 0){    //currTurnNumber is even
-                    this.players[0].mark(x,y);
-               }else this.players[1].mark(x,y);
+          if(this.currentRound <= this.rounds){   //game is ongoing
+               if(this.currTurnNumber <= 9){ //a round is ongoing
+                    if(this.currentRound%2 == 0){ //if currentRound is even
+                         if(this.currTurnNumber%2 == 0){    //currTurnNumber is even
+                              System.out.println("Player2 moves");
+                              this.players[1].mark(x,y);
+                         }else{
+                              this.players[0].mark(x,y);
+                              System.out.println("Player1 moves");
+                         }
+                    }else{    //currentRound is odd
+                         if(this.currTurnNumber%2 == 0){    //currTurnNumber is even
+                              this.players[0].mark(x,y);
+                              System.out.println("Player1 moves");
+                         }else {
+                              this.players[1].mark(x,y);
+                              System.out.println("Player2 moves");
+                         }
+                    }
+               }else{//end of round
+                    //start next round
+                    this.currentRound ++;
+                    this.currTurnNumber = 1; //back to round 1
+               }
+          }else{     //end of game, get winner
+
+          }
+
+          if(this.currTurnNumber <=9 ){
+
+
+
+          }if(this.currTurnNumber >=10){
+               this.currentRound++;
+               this.currTurnNumber = 1;
           }
      }
+
+
+     public void play(int x,int  y){
+          for(this.currentRound = this.currentRound; this.currentRound <= this.rounds; this.currentRound++){  //for each round
+               this.playerMoves(x,y);
+          }
+     }
+
+
      public void printBoard(){
           for(int i = 0; i < BOARD_HGHT; i++){
                System.out.println(this.board[i]);
